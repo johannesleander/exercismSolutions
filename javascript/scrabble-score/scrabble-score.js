2617bd53cbb40fc23fe7afc83bc324f9afc20ca8
elem <leanderjohannes@gmail.com>
2017-09-26 14:25:43 +0800

    scrabble-score

---
+++ javascript/scrabble-score/scrabble-score.js
@@ -0,0 +1,40 @@
+var scoring = {
+  A: 1,
+  B: 3,
+  C: 3,
+  D: 2,
+  E: 1,
+  F: 4,
+  G: 2,
+  H: 4,
+  I: 1,
+  J: 8,
+  K: 5,
+  L: 1,
+  M: 3,
+  N: 1,
+  O: 1,
+  Q: 10,
+  P: 3,
+  R: 1,
+  S: 1,
+  T: 1,
+  U: 1,
+  V: 4,
+  W: 4,
+  X: 8,
+  Y: 4,
+  Z: 10,
+}
+
+
+var score = (str) => {
+  if (!str || str === '') return 0;
+  let result = null;
+  for (let i = 0; i < str.length; i++) {
+    result += scoring[str[i].toUpperCase()];
+  }
+  return result;
+}
+
+module.exports = score;
