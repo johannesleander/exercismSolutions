91d5bcb402dec691a5573dd7ba7d4acf78be3ea6
elem <leanderjohannes@gmail.com>
2017-09-27 14:25:45 +0800

    strain r1

--- javascript/strain/strain.js
+++ javascript/strain/strain.js
@@ -14,22 +14,7 @@ function discard(collection, callback) {
   return result;
 }
 
-Array.prototype.remove = function() {
-    var what, a = arguments, L = a.length, ax;
-    while (L && this.length) {
-        what = a[--L];
-        while ((ax = this.indexOf(what)) !== -1) {
-            this.splice(ax, 1);
-        }
-    }
-    return this;
-};
-
 module.exports = {
   keep: keep,
   discard: discard
 }
-
-// if (value instanceof Array) {
-//   result.push(keep(value, callback));
-// } else if
