1bbe6694e5152c02e9eac265a83f3782cf664779
elem <leanderjohannes@gmail.com>
2017-09-27 15:10:38 +0800

    atbash-cipher r1

--- javascript/atbash-cipher/atbash-cipher.js
+++ javascript/atbash-cipher/atbash-cipher.js
@@ -4,18 +4,16 @@ let REGEX = /[\s,.]/g
 
 module.exports.encode = (str) => {
   str = str.replace(REGEX, "").toLowerCase();
-  console.log(str);
   let result = '';
-  let j = 0;
+  let groupIndex = 0;
   for (let i = 0; i < str.length; i++) {
-    j++;
-    if (j > 5) {
+    groupIndex++;
+    if (groupIndex > 5) {
       result += ' ';
-      j = 1;
+      groupIndex = 1;
     }
     if(/[a-z]/.test(str[i])) {
-      let index = ALPHABET.indexOf(str[i]);
-      result += ALPHABET[ALPHABET.length - index - 1];
+      result += ALPHABET[ALPHABET.length - ALPHABET.indexOf(str[i]) - 1];
     } else {
       result += str[i];
     }
